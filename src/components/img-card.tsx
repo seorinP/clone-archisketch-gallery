c943f13728af6f1219f871d27461622aa7a21dac
Seorin <seorinpark96@gmail.com>
2022-04-02 22:48:55 +0900

    feat: checkbox 전체 선택/해제 기능 추가

--- src/components/img-card.tsx
+++ src/components/img-card.tsx
@@ -5,37 +5,35 @@ import { ImgSlider } from "./img-slider";
 export function ImgCard(props: any) {
     const [isAllChecked, setIsAllChecked] = useState(false);
     const [isChecked, setIsChecked] = useState(new Array(props.renderings.length).fill(false)); // 체크 여부
-    const [checkedImages, setCheckedImages] = useState(new Set()); // 체크된 요소들
     const [visible, setVisible] = useState<boolean>(true); // 마우스 올렸을 시 메뉴 보이기
     // 참고 :
     // https://egg-programmer.tistory.com/282
     // https://www.freecodecamp.org/news/how-to-work-with-multiple-checkboxes-in-react/
 
 
-    const checkedImageHandler = (id: number, isChecked: boolean) => {
-        if (isChecked) {
-            checkedImages.add(id);
-            setCheckedImages(checkedImages);
-        } else if (!isChecked && checkedImages.has(id)) {
-            checkedImages.delete(id);
-            setCheckedImages(checkedImages);
-        }
-        console.log('checkedImages: ', checkedImages)
-    };
+    let checkedImagesCount = isChecked.filter(item => item === true).length;
 
-    const checkHandler = (position : number) => {
-        const updatedCheckedState = isChecked.map((item: any, index: number) => 
+    const checkHandler = (position: number) => {
+        const updatedCheckedState = isChecked.map((item: any, index: number) =>
             index === position ? !item : item
         );
         setIsChecked(updatedCheckedState);
     }
 
+    const checkAllHandler = (checked: boolean) => {
+        console.log('checked: ', checked)
+        setIsAllChecked(!isAllChecked)
 
+        const updatedCheckedState = isChecked.map(() =>
+            checked ? true : false
+        );
+        setIsChecked(updatedCheckedState);
+    }
 
     return (
         <>
-            <p>{isChecked.toString()}</p>
-            
+            <p>{checkedImagesCount}</p>
+            <Checkbox name='allCheck' value='allCheck' checked={isAllChecked} onChange={(e) => checkAllHandler(e.target.checked)} />
             <ImageList
                 cols={4}
                 sx={{
@@ -61,7 +59,7 @@ export function ImgCard(props: any) {
                                     setVisible(true)
 
                                 }}
-                            > 
+                            >
                                 <img
                                     style={{
                                         height: '33vh',
@@ -79,7 +77,7 @@ export function ImgCard(props: any) {
                                     position="top"
                                     actionIcon={
                                         <>
-                                            <Checkbox name='image' id={idx} value={`img-${idx}`} checked={isChecked[+idx]} onChange={()=>checkHandler(+idx)} />
+                                            <Checkbox name='image' id={idx} value={`img-${idx}`} checked={isChecked[+idx]} onChange={() => checkHandler(+idx)} />
                                         </>
                                     }
                                     actionPosition='left'
